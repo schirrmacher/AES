5f3ad23a6610139f36ae18ef4104e2e9fea9db4a
Marvin Schirrmacher <marvin.schirrmacher@rewe-digital.com>
2017-07-12 21:51:57 +0200

    fix shift-rows layer
    Shift matrix entries instead of bits.

--- src/aes-components/shift-rows.c
+++ src/aes-components/shift-rows.c
@@ -1,37 +1,40 @@
 
 #include "./shift-rows.h"
+#include <string.h>
 #include <limits.h>
 
 
-unsigned char left_shift_bits_cyclically(unsigned char value_to_shift, const unsigned int shifts);
-
 void shift_rows(unsigned char matrix[AES_MATRIX_COLUMNS][AES_MATRIX_COLUMNS]) {
+    
+    unsigned char result[AES_MATRIX_COLUMNS][AES_MATRIX_COLUMNS];
+    
     for (int row_index = 0; row_index < AES_MATRIX_ROWS; row_index++) {
         for (int column_index = 0; column_index < AES_MATRIX_COLUMNS; column_index++) {
-            const unsigned char current_entry = matrix[row_index][column_index];
-            unsigned char shifted_entry = 0;
+            
+            int shifted_column_index = column_index;
+            
             switch (row_index) {
                 case 0:
-                shifted_entry = left_shift_bits_cyclically(current_entry, 0);
-                break;
+                    // don't shift
+                    break;
                 case 1:
-                shifted_entry = left_shift_bits_cyclically(current_entry, 1);
-                break;
+                    // here one left shift: column_index - 1 + 4 mod 4 <=> column_index + 3 mod 4
+                    shifted_column_index = (column_index + 3) % AES_MATRIX_COLUMNS;
+                    break;
                 case 2:
-                shifted_entry = left_shift_bits_cyclically(current_entry, 2);
-                break;
+                    shifted_column_index = (column_index + 2) % AES_MATRIX_COLUMNS;
+                    break;
                 case 3:
-                shifted_entry = left_shift_bits_cyclically(current_entry, 3);
-                break;
+                    shifted_column_index = (column_index + 1) % AES_MATRIX_COLUMNS;
+                    break;
             }
-            matrix[row_index][column_index] = shifted_entry;
+            const unsigned char current_entry = matrix[row_index][column_index];
+            result[row_index][shifted_column_index] = current_entry;
         }
     }
-}
-
-unsigned char left_shift_bits_cyclically(unsigned char value_to_shift, const unsigned int shifts) {
-    const int normalized_shifts = shifts % CHAR_BIT;
-    const char shifted_value = value_to_shift << normalized_shifts;
-    const char overflow = value_to_shift >> (CHAR_BIT - normalized_shifts);
-    return shifted_value | overflow;
+    
+    for(int i = 0; i < AES_MATRIX_ROWS; i++) {
+        memcpy(&matrix[i], &result[i], sizeof(result[0]));
+    }
+    
 }
