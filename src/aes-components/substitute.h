49525c8c48f029f0f953618f7ee9b91c5967b3d5
Marvin Schirrmacher <marvin.schirrmacher@rewe-digital.com>
2017-07-12 20:38:39 +0200

    add substitution layer

---
+++ src/aes-components/substitute.h
@@ -0,0 +1,10 @@
+
+#ifndef substitute_h
+#define substitute_h
+
+#include <stdio.h>
+#include "aes-constants.h"
+
+void substitute(unsigned char matrix[AES_MATRIX_COLUMNS][AES_MATRIX_ROWS]);
+
+#endif /* substitute_h */
