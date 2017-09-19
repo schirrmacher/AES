df9ee4fe6f56244113cd5fcc0789a0a1829690b0
Marvin Schirrmacher <marvin.schirrmacher@rewe-digital.com>
2017-09-19 16:03:16 +0200

    transform input data to correct matrix format

--- src/main.c
+++ src/main.c
@@ -13,29 +13,32 @@
 #include "printing.h"
 
 int main(int argc, const char * argv[]) {
-    /*
+    
     uint32_t input_key[4] = {
         0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c
     };
     
     uint32_t plaintext[4] = {
         0x6bc1bee2, 0x2e409f96, 0xe93d7e11, 0x7393172a
-    };*/
-    
-    uint8_t key[4][4] = {
-        0x2b, 0x28, 0xab, 0x09,
-        0x7e, 0xae, 0xf7, 0xcf,
-        0x15, 0xd2, 0x15, 0x4f,
-        0x16, 0xa6, 0x88, 0x3c
     };
     
-    uint8_t state[4][4] = {
-        0x32, 0x88, 0x31, 0xe0,
-        0x43, 0x5a, 0x31, 0x37,
-        0xf6, 0x30, 0x98, 0x07,
-        0xa8, 0x8d, 0xa2, 0x34
-    };
-        
+    uint8_t key[4][4];
+    for (int i = 0; i < AES_STATE_MATRIX_SPAN; i++) {
+        key[0][i] = (input_key[i] >> 24) & 0xFF;
+        key[1][i] = (input_key[i] >> 16) & 0xFF;
+        key[2][i] = (input_key[i] >>  8) & 0xFF;
+        key[3][i] =  input_key[i]        & 0xFF;
+    }
+    
+    uint8_t state[4][4];
+    for (int i = 0; i < AES_STATE_MATRIX_SPAN; i++) {
+        state[0][i] = (plaintext[i] >> 24) & 0xFF;
+        state[1][i] = (plaintext[i] >> 16) & 0xFF;
+        state[2][i] = (plaintext[i] >>  8) & 0xFF;
+        state[3][i] =  plaintext[i]        & 0xFF;
+    }
+    print_state(state);
+    
     add_round_key(state, key);
     
     for (int i = 1; i <= 9; i++) {
@@ -49,18 +52,17 @@ int main(int argc, const char * argv[]) {
     shift_rows(state);
     add_round_key(state, key);
     
-    uint8_t expected[AES_STATE_MATRIX_SPAN][AES_STATE_MATRIX_SPAN] = {
-        0x39, 0x02, 0xdc, 0x19,
-        0x25, 0xdc, 0x11, 0x6a,
-        0x84, 0x09, 0x85, 0x0b,
-        0x1d, 0xfb, 0x97, 0x32
-        /*
-        0x3a, 0xd7, 0x7b, 0xb4,
-        0x0d, 0x7a, 0x36, 0x60,
-        0xa8, 0x9e, 0xca, 0xf3,
-        0x24, 0x66, 0xef, 0x97
-         */
+    uint32_t result[4] = {
+        0x3ad77bb4, 0x0d7a3660, 0xa89ecaf3, 0x2466ef97
     };
+    
+    uint8_t expected[4][4];
+    for (int i = 0; i < AES_STATE_MATRIX_SPAN; i++) {
+        expected[0][i] = (result[i] >> 24) & 0xFF;
+        expected[1][i] = (result[i] >> 16) & 0xFF;
+        expected[2][i] = (result[i] >>  8) & 0xFF;
+        expected[3][i] =  result[i]        & 0xFF;
+    }
     
     for (int i = 0; i < AES_STATE_MATRIX_SPAN; i++) {
         for (int j = 0; j < AES_STATE_MATRIX_SPAN; j++) {
