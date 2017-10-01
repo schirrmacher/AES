8bd895cf5f24eb86802f22921d3c9e40987c2ec3
Marvin Schirrmacher <marvin.schirrmacher@rewe-digital.com>
2017-10-01 21:23:23 +0200

    cleanup constants

--- src/aes-components/aes-constants.h
+++ src/aes-components/aes-constants.h
@@ -4,22 +4,21 @@
 #ifndef aes_constants_h
 #define aes_constants_h
 
-#define AES_256_ROUNDS 14
-#define AES_256_KEY_WORDS 8
-#define AES_ROUND_KEY_WORDS 4
-
-#define AES_WORD_BYTES 4
-#define AES_PLAINTEXT_WORDS 4
-#define AES_CIPHERTEXT_WORDS 4
-#define AES_STATE_SPAN 4
-
-#define AES_IRREDUCIBLE_POLYNOMIAL 0x11b
-
 typedef uint32_t block[4];
 typedef uint8_t byte;
+
+#define AES_256_ROUNDS 14
+
+#define AES_256_KEY_WORDS 8
 typedef uint32_t key[AES_256_KEY_WORDS];
-typedef byte round_key[AES_STATE_SPAN][AES_STATE_SPAN];
+
+#define AES_WORD_BYTES 4
 typedef byte word[AES_WORD_BYTES];
+
+#define AES_STATE_SPAN 4
 typedef byte state[AES_STATE_SPAN][AES_STATE_SPAN];
+typedef state round_key;
+
+#define AES_IRREDUCIBLE_POLYNOMIAL 0x11b
 
 #endif /* aes_constants_h */
