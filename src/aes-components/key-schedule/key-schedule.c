65a4d27bac8d9b9837dd2d2784625e105cca639c
Marvin Schirrmacher <marvin.schirrmacher@rewe-digital.com>
2017-10-04 08:04:33 +0200

    shorten key-schedule

--- src/aes-components/key-schedule/key-schedule.c
+++ src/aes-components/key-schedule/key-schedule.c
@@ -44,21 +44,14 @@ void inv_get_round_key(const key input_key, round_key round_key_ref) {
 
 static void compute_round_keys(const key input_key, round_key round_keys_ref[AES_256_ROUNDS + 1]) {
     
-    for (int round = 0; round < AES_256_ROUNDS + 1; round++) {
+    for (int round_key_count = 0; round_key_count < AES_256_ROUNDS + 1; round_key_count++) {
         
-        if (round == 0) {
+        if (round_key_count == 0 || round_key_count == 1) {
             
             for (int i = 0; i < AES_STATE_SPAN; i++) {
                 for (int j = 0; j < AES_STATE_SPAN; j++) {
-                    round_keys_ref[round][i][j] = (input_key[j] >> (3 - i) * 8) & 0xFF;
-                }
-            }
-            
-        } else if (round == 1) {
-            
-            for (int i = 0; i < AES_STATE_SPAN; i++) {
-                for (int j = 0; j < AES_STATE_SPAN; j++) {
-                    round_keys_ref[round][i][j] = (input_key[j + 4] >> (3 - i) * 8) & 0xFF;
+                    // parse left then right half of the input key to fit the matrix format
+                    round_keys_ref[round_key_count][i][j] = (input_key[j + (4 * round_key_count)] >> (3 - i) * 8) & 0xFF;
                 }
             }
             
@@ -66,22 +59,24 @@ static void compute_round_keys(const key input_key, round_key round_keys_ref[AES
             
             word prev_last_word;
             for (int i = 0; i < AES_WORD_BYTES; i++) {
-                prev_last_word[i] = round_keys_ref[round - 1][i][3];
+                prev_last_word[i] = round_keys_ref[round_key_count - 1][i][3];
             }
             
-            if (round % 2 == 0)
+            if (round_key_count % 2 == 0)
                 g(prev_last_word);
             else
                 h(prev_last_word);
             
             for (int i = 0; i < AES_STATE_SPAN; i++) {
-                round_keys_ref[round][i][0] = round_keys_ref[round - 2][i][0] ^ prev_last_word[i];
+                
+                round_keys_ref[round_key_count][i][0] = round_keys_ref[round_key_count - 2][i][0] ^ prev_last_word[i];
                 
                 for (int j = 1; j < AES_STATE_SPAN; j++) {
-                    round_keys_ref[round][i][j] = round_keys_ref[round][i][j - 1] ^ round_keys_ref[round - 2][i][j];
+                    round_keys_ref[round_key_count][i][j] = round_keys_ref[round_key_count][i][j - 1] ^ round_keys_ref[round_key_count - 2][i][j];
                 }
             }
             
         }
+        
     }
 }
